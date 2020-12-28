49e41fcad176f64dd976c8994e747778a980514c
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-12-28 18:18:00 +0100

    add react-helmet

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
+import { Helmet } from 'react-helmet';
 
 import Home from 'views/home';
 import Films from 'views/films';
@@ -11,6 +12,17 @@ import Search from 'views/search';
 const Root = () => {
   return (
     <MainTemplate>
+      <Helmet>
+        <title>Netflix clone</title>
+        <meta charset="utf-8" />
+        <meta name="description" content="Netflix clone app" />
+        <meta name="author" content="Rafał Stróżyk" />
+        <meta name="copyright" content="Rafał Stróżyk" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Switch>
         <Route exact path="/">
           <Home />
